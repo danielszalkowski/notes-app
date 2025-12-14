787b36d6d097fd462d88cb76100ecfc4507c1b62
Daniel Szalkowski <10836979@ieselgrao.org>
2025-12-14 21:25:50 +0100

    Fix entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,6 +5,10 @@ if [ ! -f /var/www/html/.env ]; then
     cp /var/www/html/.env.example /var/www/html/.env
 fi
 
+if ! grep -q "^APP_KEY=base64:" /var/www/html/.env; then
+    php artisan key:generate --force
+fi
+
 mkdir -p /var/www/html/database
 if [ ! -f /var/www/html/database/database.sqlite ]; then
     touch /var/www/html/database/database.sqlite
